a59f44de75252f9b0d67a46dc51742708bafae7f
Jason Shen <yujias@cs.ucla.edu>
2018-08-12 23:30:36 -0700

    add dumping to sdd files

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@
 
 #include <hierarchical_map_compiler/map_network.h>
 #include <iostream>
+extern "C" {
+#include <sdd/sddapi.h>
+}
 int main(int argc, const char* argv[]) {
-  if (argc <= 4) {
-    std::cout << "Usage: map_filename graph_hopper_script tmp_dir thread_num"
+  if (argc <= 6) {
+    std::cout << "Usage: map_filename graph_hopper_script tmp_dir thread_num "
+                 "sdd_filename vtree_filename"
               << std::endl;
     exit(1);
   }
@@ -14,10 +18,14 @@ int main(int argc, const char* argv[]) {
   std::string graph_hopper_script(argv[2]);
   std::string tmp_dir(argv[3]);
   int thread_num = atoi(argv[4]);
+  std::string sdd_filename(argv[5]);
+  std::string vtree_filename(argv[6]);
   std::cout << "Running map file : " << map_filename << std::endl;
   std::cout << "Graph hopper script : " << graph_hopper_script << std::endl;
   std::cout << "Tmp dir : " << tmp_dir << std::endl;
   std::cout << "Thread Num : " << thread_num << std::endl;
+  std::cout << "Sdd filename : " << sdd_filename << std::endl;
+  std::cout << "Vtree filename : " << vtree_filename << std::endl;
   MapNetwork* network =
       MapNetwork::MapNetworkFromJsonSpecFile(map_filename.c_str());
   network->SetGraphillionCompiler(graph_hopper_script, tmp_dir, thread_num);
@@ -27,5 +35,7 @@ int main(int argc, const char* argv[]) {
                    psdd_node_util::SerializePsddNodes(result.first))
                    .get_str(10)
             << std::endl;
+  psdd_node_util::WritePsddToFile(result.first, sdd_filename.c_str());
+  sdd_vtree_save(vtree_filename.c_str(), result.second->vtree());
   return 0;
 }
